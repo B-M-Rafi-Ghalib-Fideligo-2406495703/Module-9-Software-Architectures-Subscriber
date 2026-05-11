95de59725fe04a8b088d56978620a6eaa17dc4a3
Fideligo <mrafighalibf@gmail.com>
2026-05-11 23:32:02 +0700

    Initial subscriber code.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use borsh::{BorshDeserialize, BorshSerialize};
+use crosstown_bus::{CrosstownBus, MessageHandler, HandleError, QueueProperties};
+use std::{thread, time};
+
+#[derive(Debug, Clone, BorshDeserialize, BorshSerialize)]
+pub struct UserCreatedEventMessage {
+    pub user_id: String,
+    pub user_name: String
+}
+
+pub struct UserCreatedHandler;
+
+impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
+    fn handle(&self, message: Box<UserCreatedEventMessage>) -> Result<(), HandleError> {
+        thread::sleep(time::Duration::from_millis(1000));
+        println!("Di Komputer 2406495703. Pesan diterima: {:?}", message);
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let subscriber = CrosstownBus::new_subscriber("amqp://guest:guest@localhost:5672".to_owned()).unwrap();
+    _ = subscriber.subscribe("user_created".to_owned(), UserCreatedHandler, QueueProperties {
+        auto_delete: false,
+        durable: false,
+        use_dead_letter: true,
+        consume_queue_name: None,
+    });
+    
+    loop {
+        tokio::time::sleep(time::Duration::from_millis(1000)).await;
+    }
 }
